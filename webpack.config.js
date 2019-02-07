d021ac044437b6cc0c184ccf874b375225363baa
yamachu <yamachu.dev@gmail.com>
2019-02-07 20:46:58 +0900

    webpackでビルドできるようにする

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+const output_path = 'dist';
+
+module.exports = function(env, argv) {
+    return [
+        {
+            entry: './src/index.tsx',
+            output: {
+                path: path.resolve(__dirname, output_path),
+                filename: 'index.js',
+            },
+            resolve: {
+                extensions: ['.ts', '.tsx', '.js'],
+            },
+            devtool: argv.mode === 'production' ? false : 'source-map',
+            module: {
+                rules: [
+                    {
+                        test: /\.tsx?$/,
+                        exclude: /node_modules/,
+                        use: ['ts-loader'],
+                    },
+                    { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+                ],
+            },
+        },
+    ];
+};
